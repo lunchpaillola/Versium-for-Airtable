2480607b2ea78d744ca70f94bfe4e0b6f93a9ab1
Lola-Ojabowale <lola.ojabowale@gmail.com>
2024-04-02 22:51:07 -0400

    initial commit

---
+++ frontend/index.js
@@ -0,0 +1,9 @@
+import {initializeBlock} from '@airtable/blocks/ui';
+import React from 'react';
+
+function VersiumEnrichment() {
+    // YOUR CODE GOES HERE
+    return <div>Hello world this is the versium integration🚀</div>;
+}
+
+initializeBlock(() => <VersiumEnrichment/>);
